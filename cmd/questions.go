9f59a9d3aec6b23f9e4a867323d2eb9151cded34
Seednode <git@seedno.de>
2024-08-22 13:01:23 -0500

    Add commented out info for setting up colors

--- cmd/questions.go
+++ cmd/questions.go
@@ -20,11 +20,21 @@ import (
 type Trivia struct {
 	question string
 	answer   string
+	category string
 }
 
 type Questions struct {
 	mu   sync.RWMutex
 	list []Trivia
+}
+
+var Colors = map[string]string{
+	"Geography":         "#329cd8",
+	"Entertainment":     "#da6ab2",
+	"History":           "#e5cb3a",
+	"Arts & Literature": "#7a563c",
+	"Science & Nature":  "#157255",
+	"Sports & Leisure":  "#db6327",
 }
 
 func parseQuestions(questions string, errorChannel chan<- error) *Questions {
@@ -57,11 +67,17 @@ func parseQuestions(questions string, errorChannel chan<- error) *Questions {
 	for s.Scan() {
 		line := s.Text()
 
-		question, answer, found := strings.Cut(line, "|")
+		split := strings.Split(line, "|")
 
-		if found {
-			retVal.list = append(retVal.list, Trivia{question, answer})
+		if len(split) < 1 {
+			continue
 		}
+
+		question := split[0]
+		answer := split[1]
+		category := split[2]
+
+		retVal.list = append(retVal.list, Trivia{question, answer, category})
 	}
 
 	retVal.mu.Unlock()
@@ -75,12 +91,12 @@ func parseQuestions(questions string, errorChannel chan<- error) *Questions {
 	return &retVal
 }
 
-func getTrivia(questions *Questions) (string, string) {
+func getTrivia(questions *Questions) (string, string, string) {
 	questions.mu.RLock()
 	n := rand.IntN(len(questions.list))
 	questions.mu.RUnlock()
 
-	return questions.list[n].question, questions.list[n].answer
+	return questions.list[n].question, questions.list[n].answer, questions.list[n].category
 }
 
 func serveQuestion(questions *Questions, errorChannel chan<- error) httprouter.Handle {
@@ -96,14 +112,35 @@ func serveQuestion(questions *Questions, errorChannel chan<- error) httprouter.H
 				r.RequestURI)
 		}
 
-		question, answer := getTrivia(questions)
+		question, answer, _ := getTrivia(questions)
+
+		// color, exists := Colors[category]
+		// if !exists {
+		// 	color = "lightblue"
+		// }
 
 		var htmlBody strings.Builder
+
+		// htmlBody.WriteString(`<!DOCTYPE html><html lang="en"><head>`)
+		// htmlBody.WriteString(`<meta name="viewport" content="width=device-width, initial-scale=1">`)
+		// htmlBody.WriteString(fmt.Sprintf(`<style>html {background-color: %s; text-align: center;} a { all: unset;}`, color))
+		// htmlBody.WriteString(`p, div { font-size: clamp(var(--min), var(--val), var(--max));}`)
+		// htmlBody.WriteString(`p, div {--min: 1em; --val: 2.5vw; --max: 1.5em;}`)
+		// htmlBody.WriteString(fmt.Sprintf(`#answer {display: none;width: 100%%;padding: 50px 0;text-align: center; background-color: %s; margin-top: 20px; outline: ridge;}</style>`, "#e2dedd"))
+		// htmlBody.WriteString(fmt.Sprintf("<title>%s</title></head>", "Trivia"))
+		// htmlBody.WriteString(fmt.Sprintf(`<body><a href="/"><p id="question">%s</p></a>`, question))
+		// htmlBody.WriteString(`<button onclick="toggleAnswer()">Show Answer</button>`)
+		// htmlBody.WriteString(fmt.Sprintf(`<div id="answer">%s</div>`, answer))
+		// htmlBody.WriteString(`<script>function toggleAnswer() {var x = document.getElementById("answer");`)
+		// htmlBody.WriteString(`if (x.style.display === "block") {x.style.display = "none";} else {x.style.display = "block";}}</script>`)
+		// htmlBody.WriteString(`</body></html>`)
 
 		htmlBody.WriteString(`<!DOCTYPE html><html lang="en"><head>`)
 		htmlBody.WriteString(`<meta name="viewport" content="width=device-width, initial-scale=1">`)
 		htmlBody.WriteString(`<style>html {text-align: center;} a { all: unset;}`)
-		htmlBody.WriteString(`#answer {display: none;width: 100%;padding: 50px 0;text-align: center; background-color: lightblue; margin-top: 20px;}</style>`)
+		htmlBody.WriteString(`p, div { font-size: clamp(var(--min), var(--val), var(--max));}`)
+		htmlBody.WriteString(`p, div {--min: 1em; --val: 2.5vw; --max: 1.5em;}`)
+		htmlBody.WriteString(fmt.Sprintf(`#answer {display: none;width: 100%%;padding: 50px 0;text-align: center; background-color: %s; margin-top: 20px; outline: ridge;}</style>`, "lightblue"))
 		htmlBody.WriteString(fmt.Sprintf("<title>%s</title></head>", "Trivia"))
 		htmlBody.WriteString(fmt.Sprintf(`<body><a href="/"><p id="question">%s</p></a>`, question))
 		htmlBody.WriteString(`<button onclick="toggleAnswer()">Show Answer</button>`)
