27e708b54122d65575c29feab8f729924556cb83
Seednode <git@seedno.de>
2024-09-25 11:34:25 -0500

    Defer toggle.js loading until after page load

--- cmd/questions.go
+++ cmd/questions.go
@@ -33,27 +33,27 @@ const (
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="Description" content="A very basic trivia webapp.">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="Description" content="A very basic trivia webapp." />
 	<title>Trivia v{{.Version}}</title>
-    <link rel="stylesheet" href="/css/question.css">
+    <link rel="stylesheet" href="/css/question.css" />
 	<style nonce="{{.Nonce}}">.footer {background-color:{{.Color}};}</style>
 	<script src="/js/darkMode.js"></script>
-	<script src="/js/toggleAnswer.js"></script>
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-	<link rel="manifest" href="/favicons/site.webmanifest" crossorigin="use-credentials">
-	<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
-	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="theme-color" content="#ffffff">
-    <meta property="og:site_name" content="https://github.com/Seednode/trivia">
-    <meta property="og:title" content="Trivia v{{.Version}}">
-    <meta property="og:description" content="A very basic trivia webapp.">
-    <meta property="og:url" content="https://github.com/Seednode/trivia">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="/favicons/apple-touch-icon.png">
+	<script src="/js/toggleAnswer.js" defer></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+	<link rel="manifest" href="/favicons/site.webmanifest" crossorigin="use-credentials" />
+	<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+	<meta name="msapplication-TileColor" content="#da532c" />
+	<meta name="theme-color" content="#ffffff" />
+    <meta property="og:site_name" content="https://github.com/Seednode/trivia" />
+    <meta property="og:title" content="Trivia v{{.Version}}" />
+    <meta property="og:description" content="A very basic trivia webapp." />
+    <meta property="og:url" content="https://github.com/Seednode/trivia" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="/favicons/apple-touch-icon.png" />
   </head>
 
   <body>
--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	ReleaseVersion string = "1.1.3"
+	ReleaseVersion string = "1.1.4"
 )
 
 var (
